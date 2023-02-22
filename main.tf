c210b3e4314b91e4da95f291ccf766442b5a422d
lahirusahan1996 <99968427+lahirusahan1996@users.noreply.github.com>
2023-02-22 22:04:26 +0530

    77

--- main.tf
+++ main.tf
@@ -43,40 +43,40 @@ module "blog_sg" {
 
 }
 
-resource "aws_security_group" "blogs" {
-  name = "blogs"
+resource "aws_security_group" "blog" {
+  name = "blog"
   description = "Allow http and https in. Allow everything out"
 
   vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_security_group_rule" "blogs_http_in" {
+resource "aws_security_group_rule" "blog_http_in" {
   type        = "ingress"
   from_port   = 80
   to_port     = 80
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = aws_security_group.blogs.id
+  security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group_rule" "blogs_https_in" {
+resource "aws_security_group_rule" "blog_https_in" {
   type        = "ingress"
   from_port   = 443
   to_port     = 443
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = aws_security_group.blogs.id
+  security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group_rule" "blogs_everything_out" {
+resource "aws_security_group_rule" "blog_everything_out" {
   type        = "egress"
   from_port   = 0
   to_port     = 0
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = aws_security_group.blogs.id
+  security_group_id = aws_security_group.blog.id
 }
 
